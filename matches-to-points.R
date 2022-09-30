d6367d4c8ac58007343e4c018bde1698814da2d8
ADBond <48208438+ADBond@users.noreply.github.com>
2022-10-01 00:42:05 +0100

    more placeholder data, closer sketch of some actual script functionality

--- matches-to-points.R
+++ matches-to-points.R
@@ -2,13 +2,35 @@
 library(glue)
 library(dplyr)
 
+comp_config <- yaml::read_yaml("points-allocation.yaml")
+
 data_dir <- "./data"
 
-print("processing matches...")
+print("reading data...")
 df_matches <- readr::read_csv(glue("{data_dir}/matches.csv"))
 df_match_teams <- readr::read_csv(glue("{data_dir}/match_teams.csv"))
+df_participant_shares <- readr::read_csv(glue("{data_dir}/participant_shares.csv"))
 
+team_shares <- df_participant_shares %>%
+  group_by(team_code) %>%
+  summarise(
+    total_shares = length(share_id),
+  )
+
+print("combine and process...")
 df <- df_matches %>%
-  right_join(df_match_teams, by="match_number")
-print("Done!")
+  right_join(df_match_teams, by="match_number") %>%
+  mutate(result = tidyr::replace_na(result, "yet_to_play")) %>%
+  mutate(result_points = comp_config[["match-result-points"]][result] %>% unlist) %>%
+  mutate(multiplier = comp_config[["stage-multipliers"]][stage] %>% unlist) %>%
+  mutate(team_points = multiplier * result_points)
+
 df
+
+df %>%
+  group_by(team) %>%
+  summarise(
+    total_points = sum(team_points),
+  )
+
+print("Done!")
