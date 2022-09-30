a63d80a8aca2ad160a8d98c5cc750720681f6800
ADBond <48208438+ADBond@users.noreply.github.com>
2022-09-30 23:55:54 +0100

    script getting ready to do something useful

--- matches-to-points.R
+++ matches-to-points.R
@@ -1,4 +1,14 @@
 # stub
-df <- tibble::tibble(a = 1, b = 2)
-print("processing...")
+library(glue)
+library(dplyr)
+
+data_dir <- "./data"
+
+print("processing matches...")
+df_matches <- readr::read_csv(glue("{data_dir}/matches.csv"))
+df_match_teams <- readr::read_csv(glue("{data_dir}/match_teams.csv"))
+
+df <- df_matches %>%
+  right_join(df_match_teams, by="match_number")
+print("Done!")
 df
