c247191247b088e65f79a7a87dd0a78ce2f15afe
ADBond <48208438+ADBond@users.noreply.github.com>
2022-10-01 12:59:39 +0100

    t20 mens wc 21 as an example - group stages + dummy shares

--- matches-to-points.R
+++ matches-to-points.R
@@ -7,16 +7,12 @@ table_to_lookup <- function(df, from_to=names(df)[1:2]){
   names(lookup) <- df[[from_to[[1]]]]
   return(lookup)
 }
-divide_or_zero <- function(numerator, denominator){
-  if(denominator != 0){
-    return(numerator/denominator)
-  }
-  return(0)
-}
 
-data_dir <- "./data/mens_t20_world_cup_2022"
+data_dir <- "./data/mens_t20_world_cup_2021"
 
 comp_config <- yaml::read_yaml(glue("{data_dir}/points-allocation.yaml"))
+
+points_config <- comp_config[["points"]]
 
 print("reading data...")
 df_teams <- readr::read_csv(glue("{data_dir}/teams.csv"))
@@ -38,19 +34,20 @@ print("combine and process...")
 df <- df_matches %>%
   right_join(df_match_teams, by="match_number") %>%
   mutate(result = tidyr::replace_na(result, "yet_to_play")) %>%
-  mutate(result_points = comp_config[["match-result-points"]][result] %>% unlist) %>%
-  mutate(multiplier = comp_config[["stage-multipliers"]][stage] %>% unlist) %>%
+  mutate(result_points = points_config[["match-result-points"]][result] %>% unlist) %>%
+  mutate(multiplier = points_config[["stage-multipliers"]][stage] %>% unlist) %>%
   mutate(team_points = multiplier * result_points)
 
 df
 
 df %>%
+  filter(result != "yet_to_play") %>%
   group_by(team) %>%
   summarise(
     total_points = sum(team_points),
     .groups = "drop",
   ) %>%
   mutate(total_shares = shares_lookup[team] %>% unlist()) %>%
-  mutate(points_per_share = divide_or_zero(total_points, total_shares))
+  mutate(points_per_share = ifelse(total_shares != 0, total_points / total_shares, 0))
 
 print("Done!")
