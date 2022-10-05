9e9759d3b18d9d718c3f283c8c13eabf1b19096c
ADBond <48208438+ADBond@users.noreply.github.com>
2022-10-05 23:19:43 +0100

    injection points from yaml -> html

--- generate-html.py
+++ generate-html.py
@@ -2,7 +2,40 @@ import os
 import re
 from shutil import copy
 
+from yaml import full_load as yaml_load
+# import yaml
+
 from jinja2 import Environment, PackageLoader, select_autoescape
+
+
+# super hard-code for now
+def subs_from_yaml(yaml_file):
+    with open(yaml_file, "r") as f:
+        config = yaml_load(f.read())
+    points_obj = config["points"]
+
+    mr_bonuses = points_obj["match-result-bonuses"]
+    bonuses = points_obj["bonus-points"]
+    return {
+        "Close loss (losing with NRR less than 1 - i.e. less than 20 runs in a match going to 20 overs)": mr_bonuses["close_loss"],
+        "Bowling out opposing team": mr_bonuses["bowled_out_opponent"],
+        "Not conceding any wickets": mr_bonuses["no_wickets_lost"],
+        "Making it from the first round to super 12s": bonuses["to_super_12"],
+        "Making it to the knockout stages": bonuses["to_knockouts"],
+        "Member of team taking a hat-trick": bonuses["hat_trick"],
+        "Member of team making a century": bonuses["century"],
+        "Member of team taking a five-for": bonuses["fifer"],
+    }
+
+
+STATIC_FOLDER = "site-static-files"
+TEMPLATE_DIR = "competitions"
+SITE_DIR = "competitions-site"
+if not os.path.exists(SITE_DIR):
+    os.makedirs(SITE_DIR)
+new_static_folder = os.path.join(SITE_DIR, "static")
+if not os.path.exists(new_static_folder):
+    os.makedirs(new_static_folder)
 
 # let's just be stupid for now while i figure out a better setup
 tl_substitutions = {
@@ -16,22 +49,13 @@ tl_substitutions = {
 comp_substitutions = {
     "mens_t20_world_cup_2021": {
         "title": "Men's T20 World Cup 2021",
-        "rule_points": {"rule": 1},
+        "rule_points": subs_from_yaml(os.path.join(TEMPLATE_DIR, "mens_t20_world_cup_2021", "points-allocation.yaml")),
     },
     "mens_t20_world_cup_2022": {
         "title": "Men's T20 World Cup 2022",
         "rule_points": {"turning up": 1},
     },
 }
-
-STATIC_FOLDER = "site-static-files"
-TEMPLATE_DIR = "competitions"
-SITE_DIR = "competitions-site"
-if not os.path.exists(SITE_DIR):
-    os.makedirs(SITE_DIR)
-new_static_folder = os.path.join(SITE_DIR, "static")
-if not os.path.exists(new_static_folder):
-    os.makedirs(new_static_folder)
 
 jinja_file_regex = re.compile(r'\.jinja$')
 
