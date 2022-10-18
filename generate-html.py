3eb18a096025359a51c34cc6c19b573e14b26881
ADBond <48208438+ADBond@users.noreply.github.com>
2022-10-18 18:31:58 +0100

    leaderboard placeholder

--- generate-html.py
+++ generate-html.py
@@ -93,6 +93,7 @@ for file, subs in tl_substitutions.items():
 
 for comp, subs in comp_substitutions.items():
     comp_template = env.get_template("competition_home.jinja")
+    lb_template = env.get_template("leaderboard.jinja")
     file = f"{comp}.html"
     new_full_file = os.path.join(SITE_DIR, file)
 
@@ -109,6 +110,15 @@ for comp, subs in comp_substitutions.items():
     comp_folder = os.path.join(SITE_DIR, comp)
     if not os.path.exists(comp_folder):
         os.makedirs(comp_folder)
+    
+    lb_subs = {
+        "title": subs["title"],
+        "participant_league_table_with_links": "placeholder"
+    }
+
+    with open(os.path.join(SITE_DIR, comp, f"leaderboard.html"), "w+", encoding="utf8") as f:
+        f.write(lb_template.render(**lb_subs))
+    
     team_template = env.get_template("team_page.jinja")
     df_points = pd.read_csv(os.path.join(TEMPLATE_DIR, comp, "data", "generated", "team-points-breakdown.csv"))
     for code, team in teams.items():
@@ -133,29 +143,3 @@ for comp, subs in comp_substitutions.items():
 
 for file in os.listdir(STATIC_FOLDER):
     copy(os.path.join(STATIC_FOLDER, file), os.path.join(new_static_folder, file))
-
-# base_templates = ["base.jinja"]
-#
-# for root, dirs, files in os.walk(TEMPLATE_DIR):
-#     path = root.split(os.sep)
-#     path.append(".")
-#     subdir = os.path.join(*path[1:])
-#
-#     new_root = root.replace(TEMPLATE_DIR, SITE_DIR)
-#     for file in files:
-#         new_full_file = os.path.join(new_root, file)
-#         if not os.path.exists(new_root):
-#             os.makedirs(new_root)
-#         if jinja_file_regex.search(file):
-#             if file not in base_templates:
-#                 env = Environment(
-#                     loader=PackageLoader(TEMPLATE_DIR, subdir),
-#                     autoescape=select_autoescape()
-#                 )
-#                 template = env.get_template(file)
-#                 new_full_file = jinja_file_regex.sub(".html", new_full_file)
-#                 with open(new_full_file, "w+", encoding="utf8") as f:
-#                     f.write(template.render())
-#         else:
-#             copy(os.path.join(root, file), new_full_file)
-
